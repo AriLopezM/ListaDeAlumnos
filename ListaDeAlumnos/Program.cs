91e13ada24e33b4f62193c59defcb1a72377e9e6
AriLopezM <ian.lopez31i@std.uni.edu.ni>
2024-05-12 21:08:49 -0600

    Add project files.

---
+++ ListaDeAlumnos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeAlumnos
+{
+    public partial class Universidad
+    {
+        private List<Alumnos> listaAlumnos;
+
+        // Constructor para inicializar la lista de alumnos
+        public Universidad()
+        {
+            listaAlumnos = new List<Alumnos>();
+        }
+
+        // Método para agregar un nuevo alumno a la lista
+        public void AgregarAlumno(Alumnos alumno)
+        {
+            listaAlumnos.Add(alumno);
+        }
+
+        // Método para obtener la lista de alumnos
+        public List<Alumnos> ObtenerListaAlumnos()
+        {
+            return listaAlumnos;
+        }
+    }
+}
